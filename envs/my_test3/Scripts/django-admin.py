21dcdab2d6c8284fb65ce26e6f3af88d84f22475
aamirfly16 <aamir.fly16@gmail.com>
2021-09-16 23:12:17 +0530

    second commit

--- envs/my_test3/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:/Users/ppasha/AppData/Local/Continuum/anaconda3/envs/my_test2\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
